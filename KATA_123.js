943955df51bbfdf1fb35f640a3accc1d7a6f948d
OscarCortes05 <57568770+OscarCortes05@users.noreply.github.com>
2019-11-14 09:43:01 -0600

    Add files via upload
    Katas from CodeWars part.1

---
+++ KATA_123.js
@@ -0,0 +1,25 @@
+//KATA 1
+
+function invert(array) {
+   return array.map(e =>(e* -1));
+}
+
+const arr1 = [1,2,3,4,5]
+
+console.log(invert(arr1))
+
+//KATA 2
+
+const countSheeps=(arrayOfSheep)=>{
+  let countSheep = arrayOfSheep.filter(arrayOfSheep => arrayOfSheep === true);
+  return countSheep.length
+}
+
+//KATA 3
+
+function clean(arr) {
+  return vClear = arr.filter(arr=>arr = true);
+}
+
+
+  
